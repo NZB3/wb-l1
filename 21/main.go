8e02688a60d734030a39fc95d01264ab243094cb
NZB3 <ni.bukhalov@gmail.com>
2023-07-29 18:47:17 +0300

    21

---
+++ 21/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Type1 interface {
+	Method1()
+}
+
+type Type2 struct {
+}
+
+func (t Type2) Method2() {
+	fmt.Print("Method2")
+}
+
+type Adapter struct {
+	Type2
+}
+
+func (a Adapter) Method1() {
+	a.Method2()
+}
+
+func main() {
+	var t1 Type1
+	t1 = Adapter{}
+	t1.Method1()
+}
